f81ec9fbe072c589faffbbdcc21f2b86da224dee
TihomirPet <tihomirpetrov@web.de>
2023-02-09 12:19:48 +0100

    fix  onclick animation nav to section

--- js/OnScrollNav.js
+++ js/OnScrollNav.js
@@ -3,7 +3,7 @@ window.onscroll=()=>{
 
     if (window.scrollY>60) {
         navbar.classList.add('nav-holder-background');
-        console.log('test');
+     
     } else {
        navbar.classList.remove('nav-holder-background');  
     }
