4ba3142190977e3b23d55b89fad82df03ac8075a
TihomirPet <tihomirpetrov@web.de>
2023-02-27 13:57:34 +0100

    test

--- js/Nav.js
+++ js/Nav.js
@@ -1,31 +1,31 @@
 // const infoHtml = document.querySelector('.navhold');
-const subject = document.querySelector('#navhold');
-const links = [
-  { href: '#01', name: 'Home' },
-  { href: '#02', name: 'Profil' },
-  { href: '#03', name: 'Projekte' },
-  { href: '#04', name: 'Kontakt' },
-];
+// const subject = document.querySelector('#navhold');
+// const links = [
+//   { href: '#01', name: 'Home' },
+//   { href: '#02', name: 'Profil' },
+//   { href: '#03', name: 'Projekte' },
+//   { href: '#04', name: 'Kontakt' },
+// ];
 
-const infoHtml = `
+// const infoHtml = `
      
 
 
- <nav class="nav font-Comfortaa-light font-12">
+//  <nav class="nav font-Comfortaa-light font-12">
 
-    ${links.reduce(
-      (html, value) =>
-        html +
-        `     <li class="nav-item animateNav">
-               <a class="nav-link "  href="${value.href}">${value.name}</a>
-              </li>
+//     ${links.reduce(
+//       (html, value) =>
+//         html +
+//         `     <li class="nav-item animateNav">
+//                <a class="nav-link "  href="${value.href}">${value.name}</a>
+//               </li>
               
-              `,
-      ''
-    )}
+//               `,
+//       ''
+//     )}
 
        
- </nav> 
+//  </nav> 
        
    
 
@@ -33,6 +33,6 @@ const infoHtml = `
   
  
  
-`;
+// `;
 
-subject.insertAdjacentHTML('beforeend', infoHtml);
+// subject.insertAdjacentHTML('beforeend', infoHtml);
